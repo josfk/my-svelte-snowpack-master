c306bc335e008d7c162c471b2f84e87b5e299d45
Unknown <josf.k@outlook.es>
2021-02-15 12:22:14 +0100

    first commit

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<!-- App.svelte -->
+<script>
+  /* component logic will go here */
+  import Nav from "./components/Nav.svelte";
+  import List from "./components/List.svelte";
+</script>
+
+<style>
+  /* css will go here */
+</style>
+
+<main>
+  <div class="App">
+    <Nav />
+    <h1>I changed!</h1>
+    <List />
+  </div>
+</main>
